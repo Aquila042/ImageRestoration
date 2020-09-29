d88080ed60c7f1df77331e00896d8d8408805552
JonathanWidov <JonathanWidov@live.com>
2020-09-29 02:07:29 +0200

    SOR seems to work now

--- SOR.py
+++ SOR.py
@@ -9,33 +9,37 @@ Takes the mask indicies and values from neighbours and returns a list consisting
 import numpy as np
 from random import randint
 def SOR(maskIndex, neighbours,n,omega): 
-    iterations=np.linspace(1,n,n)
-    maximumIndex=len(maskIndex)
+    maximumIndex=(maskIndex[-1]+1)
     restored=np.zeros(maximumIndex) # restored consists of index for the interior point restored and the second list is the values for these points
-    rho=0.9
-    h=1/n
-    for a in range(len(maskIndex)):     #assigning random values
-        restored[a]=randint(1,255)
+    for a in maskIndex:     #assigning random values
+        restored[a]=randint(150,170)
         
         
 # This thing never checks if they have been used previously.
 # it simply uses the values from the restored list which are initialised and then updated.
 
     
-    for i in iterations:  
+#    for i in iterations:  
+#        for t in range(len(maskIndex)):
+#            if len(neighbours[t][0]) > 3:
+#                restored[t]=(1-omega)*(restored[t])*(omega/4)*((restored[(neighbours[t][0][0])])+(restored[(neighbours[t][0][1])])+(restored[(neighbours[t][0][2])])+(restored[(neighbours[t][0][3])])-rho*h**2)                
+#            elif len(neighbours[t][0]) > 2: 
+#                restored[t]=(1-omega)*(restored[t])*(omega/3)*((restored[(neighbours[t][0][0])])+(restored[(neighbours[t][0][1])])+(restored[(neighbours[t][0][2])])+sum(neighbours[t][1])-rho*h**2)            
+#            elif len(neighbours[t][0]) > 1: 
+#                restored[t]=(1-omega)*(restored[t])*(omega/2)*((restored[(neighbours[t][0][0])])+(restored[(neighbours[t][0][1])])+sum(neighbours[t][1])-rho*h**2)            
+#           elif len(neighbours[t][0]) > 0:
+#               restored[t]=(1-omega)*(restored[t])*(omega/1)*((restored[(neighbours[t][0][0])])+sum(neighbours[t][1])-rho*h**2)
+#            else:
+#                break
+    for i in range(n):
         for t in range(len(maskIndex)):
-            if len(neighbours[t][0]) > 3:
-                restored[t]=(1-omega)*(restored[t])*(omega/4)*((restored[(neighbours[t][0][0])])+(restored[(neighbours[t][0][1])])+(restored[(neighbours[t][0][2])])+(restored[(neighbours[t][0][3])])-rho*h**2)                
-            elif len(neighbours[t][0]) > 2: 
-                restored[t]=(1-omega)*(restored[t])*(omega/3)*((restored[(neighbours[t][0][0])])+(restored[(neighbours[t][0][1])])+(restored[(neighbours[t][0][2])])+sum(neighbours[t][1])-rho*h**2)            
-            elif len(neighbours[t][0]) > 1: 
-                restored[t]=(1-omega)*(restored[t])*(omega/2)*((restored[(neighbours[t][0][0])])+(restored[(neighbours[t][0][1])])+sum(neighbours[t][1])-rho*h**2)            
-            elif len(neighbours[t][0]) > 0:
-                restored[t]=(1-omega)*(restored[t])*(omega/1)*((restored[(neighbours[t][0][0])])+sum(neighbours[t][1])-rho*h**2)
-            else:
-                break
-                
- #   restored = [x for x in restored if x !=0]# this is merely used for a check and should be removed later.
+            Closest=[]
+            for i in range(len(neighbours[t][0])):
+                Closest.append(restored[neighbours[t][0][i]])
+            for p in range(len(neighbours[t][1])):
+                Closest.append(neighbours[t][1][p])
+            restored[t]=((1-omega)*restored[t]+(omega/(4-len(neighbours[t][1])))*sum(Closest))    
+    restored = [x for x in restored if x !=0]# this is merely used for a check and should be removed later.
     return restored 
 
 def RestoreIndex(imageMatrix,restored,maskIndex):
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import utils as utl
 from mask import findNeighbours
 
-testMask = utl.makeMask("masks/128/3x3.png")
+
+testMask = utl.makeMask("masks/128/smile.jpeg")
 
 testImage = utl.loadImage("pictures/128/001.jpeg")
 
 testN = findNeighbours(testImage, testMask[1])
-
-print(testN)
+from SOR import SOR
+testR = SOR(testMask[1],testN,100,1.9)#the two ones are the number of iterations and the relaxation constant.
+#print(len(testR))
+#print(testN)
+#print(len(testMask[1]))
+#print(len(testN))
+print(testR)
