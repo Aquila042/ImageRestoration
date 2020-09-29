e1ebd9eebd12707de05d7ca320c2b74c8486a409
JonathanWidov <JonathanWidov@live.com>
2020-09-29 22:15:39 +0200

    some testing in main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ testImage = utl.loadImage("pictures/128/001.jpeg")
 
 testN = findNeighbours(testImage, testMask[1])
 from SOR import SOR
-testR = SOR(testMask[1],testN,100,1.9)#the two ones are the number of iterations and the relaxation constant.
+testR = SOR(testMask[1],testN,15,1.9)#the two ones are the number of iterations and the relaxation constant.
 #print(len(testR))
 #print(testN)
 #print(len(testMask[1]))
