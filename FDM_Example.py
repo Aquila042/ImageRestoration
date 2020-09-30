dc956ca220257c3f5d8d454028612d018f7b779c
Aquila042 <39123979+Aquila042@users.noreply.github.com>
2020-09-30 16:40:54 +0200

    Added uppscale example in FDM_Example

--- FDM_Example.py
+++ FDM_Example.py
@@ -1,13 +1,12 @@
 import utils as utl
-from mask import findNeighbours
 import numpy as np
 
 
 testMask = utl.makeMask("masks/128/circles.png")
 
 testImage = utl.loadImage("pictures/128/005.jpeg")
-Image = np.copy(testImage)
-testN = findNeighbours(testImage, testMask[1])
+image = np.copy(testImage)
+testN = utl.findNeighbours(testImage, testMask[1])
 from SOR import SOR, RestoreIndex
 testR = SOR(testMask[1],testN,30,1.9)#the two ones are the number of iterations and the relaxation constant.
 #print(len(testR))
@@ -16,8 +15,27 @@ testR = SOR(testMask[1],testN,30,1.9)#the two ones are the number of iterations 
 #print(len(testN))
 #print(testR)
 
-FixedImg = np.copy(RestoreIndex(Image, testR, testMask[1]))
+FixedImg = np.copy(RestoreIndex(image, testR, testMask[1]))
 utl.showImage(FixedImg)
-utl.showImage(Image)
-DS = utl.discrepancyScore(Image, FixedImg, testMask[1])
+utl.showImage(image)
+DS = utl.discrepancyScore(image, FixedImg, testMask[1])
 print(DS)
+
+
+#%%
+#Uppscale example
+from PIL import Image, ImageOps
+
+orgImage = utl.loadImage("pictures/128/002.jpeg")
+
+upImage = Image.fromarray(orgImage.astype(np.uint8))
+upImage = upImage.resize((252, 252))
+upImagePad = ImageOps.expand(upImage, border=3, fill ="black")
+upScaled = np.copy(np.array(upImagePad))
+
+mesh = utl.makeMask("masks/256/upscaling_grid_pad.png")
+mechN = utl.findNeighbours(upScaled, mesh[1])
+
+smooth = SOR(mesh[1], mechN, 10, 1.9)
+doubleSizeIm = np.copy(RestoreIndex(upScaled, smooth, mesh[1]))
+utl.showImage(doubleSizeIm)
